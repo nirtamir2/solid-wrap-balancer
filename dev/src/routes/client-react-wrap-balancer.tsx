2e94f340741fb04a3e4f249acc0b7b753443c9a7
Nir Tamir <nirtamir2@gmail.com>
2023-01-14 12:51:39 +0200

    remove spring and cleanup pages

--- dev/src/routes/client-react-wrap-balancer.tsx
+++
@@ -1,8 +0,0 @@
-import { unstable_clientOnly } from "solid-start";
-
-const ClientOnlyPage = unstable_clientOnly(async () => {
-  return await import("./react-wrap-balancer");
-});
-export default function ClientReactWrapBalancerPage() {
-  return <ClientOnlyPage />;
-}
