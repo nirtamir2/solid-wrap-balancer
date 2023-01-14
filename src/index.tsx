aaa9882b6375a833de8def5efec824876915e050
Nir Tamir <nirtamir2@gmail.com>
2023-01-14 13:25:58 +0200

    remove lib diff

--- src/index.tsx
+++ src/index.tsx
@@ -92,7 +92,7 @@ const relayout: RelayoutFn = (
   // the function.
   if (wrapper.__wrap_o == null) {
     (wrapper.__wrap_o = new ResizeObserver(() => {
-      window.__wrap_b(0, +wrapper.dataset.brr!, wrapper);
+      self.__wrap_b(0, +wrapper.dataset.brr!, wrapper);
     })).observe(container);
   }
 };
@@ -102,7 +102,7 @@ const RELAYOUT_STR = relayout.toString();
 const createScriptElement = (injected: boolean, suffix = "") => (
   <script
     // Calculate the balance initially for SSR
-    innerHTML={(injected ? "" : `window.${SYMBOL_KEY}=${RELAYOUT_STR};`) + suffix}
+    innerHTML={(injected ? "" : `self.${SYMBOL_KEY}=${RELAYOUT_STR};`) + suffix}
   />
 );
 
@@ -145,7 +145,7 @@ export function Balancer(_props: BalancerProps) {
       return;
     }
     // Re-assign the function here as the component can be dynamically rendered, and script tag won't work in that case.
-    (window[SYMBOL_KEY] = relayout)(0, props.ratio, wrapper);
+    (self[SYMBOL_KEY] = relayout)(0, props.ratio, wrapper);
   });
 
   // Remove the observer when unmounting.
@@ -182,7 +182,7 @@ export function Balancer(_props: BalancerProps) {
       </Dynamic>
       {createScriptElement(
         hasProvider,
-        `window.${SYMBOL_KEY}("${id}",${props.ratio})`
+        `self.${SYMBOL_KEY}("${id}",${props.ratio})`
       )}
     </>
   );
